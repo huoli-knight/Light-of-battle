755e6a6e55ccae49799d47c3c03c26054251c599
huoli-knight <158906158@qq.com>
2019-04-26 23:20:48 +0800

    set index back

--- src/com/huoli/Index/Index.java
+++ src/com/huoli/Index/Index.java
@@ -1,38 +1,73 @@
 package com.huoli.Index;
 
 import java.awt.Color;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
 
-import com.huoli.Second.Second;
+import com.huoli.BaseJFrame.BaseJFrame;
+import com.huoli.Menu.Menu;
 
-public class Index implements ActionListener {
+@SuppressWarnings("serial")
+public class Index extends BaseJFrame {
 
-	private JFrame index;
+	private ImageIcon imageback;
+	private JPanel panelone;
+	private JPanel panelback;
+	private JLabel back;
+	private JButton enterbutton;
 	private JButton button;
 
 	public Index() {
-		index = new JFrame("Index");
-		button = new JButton("跳转");
-		index.add(button);
-		button.addActionListener(this);
+		this.setTitle("Index");
+		lay = new JLayeredPane();
+		panelone = new JPanel();
+		panelback = new JPanel();
+		imageback = new ImageIcon("./static/indexback.gif");
+		back = new JLabel(imageback);
+		enterbutton = new JButton("进入");
+		button = new JButton("开始游戏");
 	}
 
 	public void run() {
-		Color background = new Color(11);
-		index.setBackground(background);
-		index.setSize(200, 200);
-		index.setVisible(true);
+		panelone.add(enterbutton);
+		enterbutton.addActionListener(this);
+		imageback.setImage(imageback.getImage().getScaledInstance(800, 800, Image.SCALE_DEFAULT));
+		panelback.setSize(800,800);
+		back.setSize(800,800);
+		panelback.add(back);
+		button.setBackground(Color.yellow);
+		panelone.add(button);
+		button.addActionListener(this);
+		lay.add(panelback,JLayeredPane.DEFAULT_LAYER);
+		lay.add(panelone,JLayeredPane.MODAL_LAYER);
+		this.setLayeredPane(lay);
+		panelone.setSize(100, 100);
+		panelone.setLocation(400,200);
+		this.setLayeredPane(lay);
+		this.setSize(800,800);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocation(500,200);
+		this.setResizable(false);
+		this.setVisible(true);
 	}
-	
+
 	@Override
-	public void actionPerformed (ActionEvent e) {
-		index.setVisible(false);
-		Second next = new Second();
-		next.run();
+	public void actionPerformed(ActionEvent e) {
+		String state = e.getActionCommand();
+		if (state.equals("进入")) {
+		}
+		else {
+			this.setVisible(false);
+			Menu next = new Menu();
+			next.run();
+		}
 	}
 
 }
